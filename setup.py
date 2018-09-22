b5cd8d5849749fd489ecd6cd0e00763b79ddf34c
Mohan Kishore <mohanki@amazon.com>
2018-09-21 20:37:00 -0700

    Only supporting Python 3.6+

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
